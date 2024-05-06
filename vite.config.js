02dcaa4d7ed3c68a46a0f53d84c3a577f0d1fe68
Christopher Le Boeuf <cleb0002@student.monash.edu>
2024-05-06 13:34:18 +1000

    Changed Vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'portfolio-website/',
+  base: '/portfolio-website/',
   plugins: [react()],
   assetsInclude: ['**/*.glb'],
 })
